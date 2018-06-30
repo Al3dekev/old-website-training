8809a10ff66e7deb8a35da58bf403dd91d5f0bdc
Al3dekev <q.basset@icloud.com>
2018-06-30 11:44:39 +0200

    feature: Ajout des fichiers de projet

---
+++ connexionBDD/connect.php
@@ -0,0 +1,23 @@
+﻿
+<?php
+
+
+	$db_name = '';
+	$db_host = 'localhost';
+	$db_user = 'root';
+	$db_pass = '';
+
+
+	
+try{
+	$connection = new PDO("mysql:host=$db_host;dbname=$db_name",$db_user,$db_pass);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br>";
+    die();
+}
+
+
+
+?>
